a76d5a3de963302c6bb38e227479f51a41e5101d
juntingh <junting.huang@gmail.com>
2017-04-17 23:33:23 -0500

    cleaned up NovaGeneralPhysics

--- include/NovaGeneralPhysics.hh
+++ include/NovaGeneralPhysics.hh
@@ -3,29 +3,19 @@
 
 #include "globals.hh"
 #include "G4ios.hh"
-
 #include "G4VPhysicsConstructor.hh"
-
 #include "G4Decay.hh"
+
 
 class NovaGeneralPhysics : public G4VPhysicsConstructor
 {
   public:
-
     NovaGeneralPhysics(const G4String& name = "general");
     virtual ~NovaGeneralPhysics();
-
-    // This method will be invoked in the Construct() method.
-    // each particle type will be instantiated
     virtual void ConstructParticle();
- 
-    // This method will be invoked in the Construct() method.
-    // each physics process will be instantiated and
-    // registered to the process manager of each particle type
     virtual void ConstructProcess();
 
   protected:
-
     G4Decay* fDecayProcess;
 };
 
--- src/NovaGeneralPhysics.cc
+++ src/NovaGeneralPhysics.cc
@@ -1,44 +1,32 @@
 #include "NovaGeneralPhysics.hh"
+#include "G4ProcessManager.hh"
+#include "G4ChargedGeantino.hh"
+#include "G4Geantino.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 NovaGeneralPhysics::NovaGeneralPhysics(const G4String& name)
-                     :  G4VPhysicsConstructor(name) {}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    : G4VPhysicsConstructor(name) {}
 
 NovaGeneralPhysics::~NovaGeneralPhysics() {
   fDecayProcess = NULL;
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-#include "G4ProcessManager.hh"
-// Bosons
-#include "G4ChargedGeantino.hh"
-#include "G4Geantino.hh"
-
 void NovaGeneralPhysics::ConstructParticle()
 {
-  // pseudo-particles
   G4Geantino::GeantinoDefinition();
   G4ChargedGeantino::ChargedGeantinoDefinition();
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 void NovaGeneralPhysics::ConstructProcess()
 {
   fDecayProcess = new G4Decay();
-
-  // Add Decay Process
   aParticleIterator->reset();
+
   while( (*aParticleIterator)() ){
     G4ParticleDefinition* particle = aParticleIterator->value();
     G4ProcessManager* pmanager = particle->GetProcessManager();
     if (fDecayProcess->IsApplicable(*particle)) {
       pmanager ->AddProcess(fDecayProcess);
-      // set ordering for PostStepDoIt and AtRestDoIt
       pmanager ->SetProcessOrdering(fDecayProcess, idxPostStep);
       pmanager ->SetProcessOrdering(fDecayProcess, idxAtRest);
     }
