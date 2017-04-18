30f850907f79821965112277943ca56182f6042e
juntingh_imac <junting.huang@gmail.com>
2017-04-18 10:23:25 -0500

    cleaned up NovaSteppingAction

--- src/NovaSteppingAction.cc
+++ src/NovaSteppingAction.cc
@@ -21,20 +21,19 @@ NovaSteppingAction::NovaSteppingAction(NovaRecorderBase* r)
 
 NovaSteppingAction::~NovaSteppingAction() {}
 
-void NovaSteppingAction::UserSteppingAction(const G4Step* theStep){
+void NovaSteppingAction::UserSteppingAction(const G4Step* theStep)
+{
   G4Track* theTrack = theStep->GetTrack();
 
   NovaUserTrackInformation* trackInformation = (NovaUserTrackInformation*) theTrack->GetUserInformation();
   NovaUserEventInformation* eventInformation
       = (NovaUserEventInformation*)G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetUserInformation();
 
-  G4StepPoint* thePrePoint = theStep->GetPreStepPoint();
-  G4VPhysicalVolume* thePrePV = thePrePoint->GetPhysicalVolume();
+  G4StepPoint* preStepPoint = theStep->GetPreStepPoint();
+  G4VPhysicalVolume* prePhysicalVolume = preStepPoint->GetPhysicalVolume();
+  G4StepPoint* postStepPoint = theStep->GetPostStepPoint();
+  G4VPhysicalVolume* postPhysicalVolume = postStepPoint->GetPhysicalVolume();
 
-  G4StepPoint* thePostPoint = theStep->GetPostStepPoint();
-  G4VPhysicalVolume* thePostPV = thePostPoint->GetPhysicalVolume();
-
-  G4OpBoundaryProcessStatus boundaryStatus = Undefined;
   static G4OpBoundaryProcess* boundary = NULL;
 
   if (!boundary) {
@@ -42,47 +41,25 @@ void NovaSteppingAction::UserSteppingAction(const G4Step* theStep){
     G4int processCount = processManager->GetProcessListLength();
     G4ProcessVector* processes = processManager->GetProcessList();
     for (G4int i = 0; i < processCount; i++) {
-      if((*processes)[i]->GetProcessName() == "OpBoundary"){
+      if ((*processes)[i]->GetProcessName() == "OpBoundary") {
         boundary = (G4OpBoundaryProcess*)(*processes)[i];
         break;
       }
     }
   }
 
-  if (theTrack->GetParentID() == 0) {
-    G4TrackVector* fSecondary = fpSteppingManager->GetfSecondary();
-    G4int fN2ndariesSum = fpSteppingManager->GetfN2ndariesAtRestDoIt()
-                          + fpSteppingManager->GetfN2ndariesAlongStepDoIt()
-                          + fpSteppingManager->GetfN2ndariesPostStepDoIt();
-
-    if (!eventInformation->isConversionPositionSet() && fN2ndariesSum > 0) {
-      for(size_t lp1=(*fSecondary).size()-fN2ndariesSum; lp1<(*fSecondary).size(); lp1++){
-        const G4VProcess* creator=(*fSecondary)[lp1]->GetCreatorProcess();
-        if (creator) {
-          G4String creatorName=creator->GetProcessName();
-          if(creatorName == "phot"|| creatorName == "compt" || creatorName == "conv"){
-            eventInformation->setConversionPosition((*fSecondary)[lp1]->GetPosition());
-          }
-        }
-      }
-    }
-
-    if(fOneStepPrimaries && thePrePV->GetName() == "scintillator")
-      theTrack->SetTrackStatus(fStopAndKill);
-  }
-
-  if (!thePostPV)
+  if (!postPhysicalVolume)
     return;
 
   G4ParticleDefinition* particleType = theTrack->GetDefinition();
   if (particleType == G4OpticalPhoton::OpticalPhotonDefinition()) {
-    if (thePostPoint->GetProcessDefinedStep()->GetProcessName() == "OpAbsorption") {
-      eventInformation->incrementAbsorption();
+    if (postStepPoint->GetProcessDefinedStep()->GetProcessName() == "OpAbsorption") {
       trackInformation->addTrackStatusFlag(absorbed);
+      eventInformation->incrementAbsorption();
     }
 
-    boundaryStatus = boundary->GetStatus();
-    if (thePostPoint->GetStepStatus() == fGeomBoundary) {
+    G4OpBoundaryProcessStatus boundaryStatus = boundary->GetStatus();
+    if (postStepPoint->GetStepStatus() == fGeomBoundary) {
       fExpectedNextStatus=Undefined;
       switch(boundaryStatus){
         case Absorption:
@@ -98,7 +75,7 @@ void NovaSteppingAction::UserSteppingAction(const G4Step* theStep){
           break;
         case FresnelReflection:
         case TotalInternalReflection:
-          if(thePrePV->GetName() == "fibcore")
+          if(prePhysicalVolume->GetName() == "fiberCore")
             trackInformation->incrementTotalInternalReflectionCount();
           break;
         case LambertianReflection:
@@ -106,15 +83,14 @@ void NovaSteppingAction::UserSteppingAction(const G4Step* theStep){
         case SpikeReflection:
         case BackScattering:
           trackInformation->incrementReflectionCount();
-          fExpectedNextStatus=StepTooSmall;
+          fExpectedNextStatus = StepTooSmall;
           break;
         default:
           break;
       }
-      if(thePostPV->GetName()=="sphere")
-        trackInformation->addTrackStatusFlag(hitSphere);
     }
   }
 
-  if(fRecorder)fRecorder->RecordStep(theStep);
+  if(fRecorder)
+    fRecorder->RecordStep(theStep);
 }
