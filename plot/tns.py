25daa0582fbaa13c4af83dbcbf4a4f36c4cc8aa2
juntingh <junting.huang@gmail.com>
2017-05-31 17:13:43 -0500

    cleaned up the module, mamke pretty plots for the slides

--- plot/tns.py
+++ plot/tns.py
@@ -39,10 +39,10 @@ def get_h1_slice_durations(filename):
 
 def plot_slice_durations():
     h1 = get_h1_slice_durations('tns40.txt')
-    # h2 = get_h1_slice_durations('tns35.txt')
+    h2 = get_h1_slice_durations('tns35.txt')
     h3 = get_h1_slice_durations('tns41.txt')
 
-    # h1.Scale(h3.Integral() / h1.Integral())
+    h1.Scale(h3.Integral() / h1.Integral())
     h1.Scale(float(len(get_event_tnss('tns41.txt'))) / len(get_event_tnss('tns40.txt')))
 
     c1 = TCanvas('c1', 'c1', 800, 600)
@@ -51,9 +51,9 @@ def plot_slice_durations():
     h1.SetName('h1')
     h1.Draw()
 
-    # h2.SetLineColor(kBlue)
-    # h2.SetName('h2')
-    # h2.Draw('sames')
+    h2.SetLineColor(kBlue)
+    h2.SetName('h2')
+    h2.Draw('sames')
 
     h3.SetLineColor(kRed)
     h3.SetName('h3')
@@ -72,16 +72,9 @@ def get_h1_cell_tns(filename):
             
     return h_cell_tns
 
-def get_h1_absolute_cell_tns(filename):
-    h_absolute_cell_tns = TH1D('h_absolute_cell_tns', 'h_absolute_cell_tns', 100, 210000, 230000)
-    for cell_tnss in get_slice_tnss(filename):
-        for cell_tns in cell_tnss:
-            h_absolute_cell_tns.Fill(cell_tns)
-    return h_absolute_cell_tns
-
 def plot_cell_tns():
     h1 = get_h1_cell_tns('tns40.txt')
-    # h2 = get_h1_cell_tns('tns37.txt')
+    h2 = get_h1_cell_tns('tns37.txt')
     h3 = get_h1_cell_tns('tns41.txt')
 
     c1 = TCanvas('c1', 'c1', 800, 600)
@@ -90,9 +83,9 @@ def plot_cell_tns():
     h1.GetYaxis().SetRangeUser(0, h1.GetMaximum() * 1.2)
     h1.Draw()
 
-    # h2.SetLineColor(kBlue)
-    # h2.SetName('h2')
-    # h2.Draw('sames')
+    h2.SetLineColor(kBlue)
+    h2.SetName('h2')
+    h2.Draw('sames')
 
     h3.SetLineColor(kRed)
     h3.SetName('h3')
@@ -100,6 +93,13 @@ def plot_cell_tns():
     
     c1.Update()
     raw_input('Press anykey to continue.\n')
+
+def get_h1_absolute_cell_tns(filename):
+    h_absolute_cell_tns = TH1D('h_absolute_cell_tns', 'h_absolute_cell_tns', 100, 210000, 230000)
+    for cell_tnss in get_slice_tnss(filename):
+        for cell_tns in cell_tnss:
+            h_absolute_cell_tns.Fill(cell_tns)
+    return h_absolute_cell_tns
 
 def plot_h1_absolute_cell_tns():
     h1 = get_h1_absolute_cell_tns('tns36.txt')
@@ -130,144 +130,174 @@ def print_run_info(filename):
         for j, slice_tns in enumerate(event_tns):
             print 'slice {} has {} cell hits, with mean {}'.format(j, len(slice_tns), sum(slice_tns) / len(slice_tns))
 
-def plot_slice_durations_from_hist():
-    f1 = TFile('photon_timing_mc.40.root')
-    h_slice_duration_1 = f1.Get('photontimingana/fSliceDuration')
-    h_slice_count_1 = f1.Get('photontimingana/fSliceCount')
-    event_count_1 = h_slice_count_1.GetEntries()
+def plot_effect_of_20_wls():
+    gStyle.SetOptStat('nmri')
 
-    f2 = TFile('photon_timing_mc.41.root')
-    h_slice_duration_2 = f2.Get('photontimingana/fSliceDuration')
-    h_slice_count_2 = f2.Get('photontimingana/fSliceCount')
-    event_count_2 = h_slice_count_2.GetEntries()
+    f_mc_20wls = TFile('data/photon_timing_mc.40.root')
+    h_mc_20wls = f_mc_20wls.Get('photontimingana/fSliceDuration')
+    h_mc_20wls_slice_count = f_mc_20wls.Get('photontimingana/fSliceCount')
+    mc_20wls_event_count = h_mc_20wls_slice_count.GetEntries()
+
+    f_mc = TFile('data/photon_timing_mc.45.root')
+    h_mc = f_mc.Get('photontimingana/fSliceDuration')
+    h_mc_slice_count = f_mc.Get('photontimingana/fSliceCount')
+    mc_event_count = h_mc_slice_count.GetEntries()
 
     f_data = TFile('data/photon_timing_data_1.root')
-    h_slice_duration_data = f_data.Get("photontimingana/fSliceDuration")
-    h_slice_count_data = f_data.Get("photontimingana/fSliceCount")
-    event_count_data = h_slice_count_data.GetEntries()
+    h_data = f_data.Get("photontimingana/fSliceDuration")
+    h_data_slice_count = f_data.Get("photontimingana/fSliceCount")
+    data_event_count = h_data_slice_count.GetEntries()
 
-    h_slice_duration_1.Scale(float(event_count_2) / event_count_1)
-    h_slice_duration_data.Scale(float(event_count_2) / event_count_data)
+    h_mc_20wls.Rebin(2)
+    h_mc.Rebin(2)
+    h_data.Rebin(2)
+
+    print 'mc_20wls_event_count = ', mc_20wls_event_count
+    print 'mc_event_count = ', mc_event_count
+    print 'data_event_count = ', data_event_count
+
+    h_mc_20wls.Scale(float(mc_event_count) / mc_20wls_event_count)
+    h_data.Scale(float(mc_event_count) / data_event_count)
 
     c1 = TCanvas('c1', 'c1', 800, 600)
     set_margin()
 
-    set_h1_style(h_slice_duration_1)
-    h_slice_duration_1.SetLineColor(kBlue + 2)
-    h_slice_duration_1.SetName('h1')
-    h_slice_duration_1.GetXaxis().SetTitle('Slice Duration (ns)')
-    h_slice_duration_1.GetYaxis().SetTitle('Slice Count')
-    h_slice_duration_1.Draw('hist')
+    set_h1_style(h_mc_20wls)
+    h_mc_20wls.GetYaxis().SetRangeUser(0, 1200)
+    h_mc_20wls.SetLineColor(kRed + 2)
+    h_mc_20wls.SetLineWidth(3)
+    h_mc_20wls.SetName('h1')
+    h_mc_20wls.GetXaxis().SetTitle('Slice Duration (ns)')
+    h_mc_20wls.GetYaxis().SetTitle('Slice Count')
+    h_mc_20wls.SetName('MC with 20 WLS')
+    h_mc_20wls.Draw('hist')
 
-    set_h1_style(h_slice_duration_2)
-    h_slice_duration_2.SetLineColor(kGreen + 2)
-    h_slice_duration_2.Draw('sames, hist')
+    set_h1_style(h_mc)
+    h_mc.SetLineColor(kBlue + 2)
+    h_mc_20wls.SetLineWidth(3)
+    h_mc.SetName('Default MC')
+    h_mc.Draw('sames, hist')
 
-    set_h1_style(h_slice_duration_data)
-    h_slice_duration_data.SetLineColor(kRed + 2)
-    h_slice_duration_data.Draw('sames')
-
-    lg1 = TLegend(0.5, 0.6, 0.85, 0.85)
-    lg1.AddEntry(h_slice_duration_data, 'Default Data', 'l')
-    lg1.AddEntry(h_slice_duration_2, 'Default MC', 'l')
-    lg1.AddEntry(h_slice_duration_1, 'MC with 20 WLS', 'l')
-    set_legend_style(lg1)
-    lg1.Draw()
+    set_h1_style(h_data)
+    h_data.SetLineColor(kGreen + 2)
+    h_data.SetName('Default Data')
+    h_data.Draw('sames')
 
     c1.Update()
-    c1.SaveAs('figures/plot_slice_durations_from_hist.pdf')
+    p_data = h_data.GetListOfFunctions().FindObject("stats")
+    p_data.SetTextColor(kGreen + 2)
+    p_data.SetLineColor(kGreen + 2)
+    p_data.SetX1NDC(0.72)
+    p_data.SetY1NDC(0.75)
+    p_data.SetX2NDC(0.95)
+    p_data.SetY2NDC(0.95)
+    p_data.Draw()
+
+    p_mc = h_mc.GetListOfFunctions().FindObject("stats")
+    p_mc.SetTextColor(kBlue + 2)
+    p_mc.SetLineColor(kBlue + 2)
+    p_mc.SetX1NDC(0.72)
+    p_mc.SetY1NDC(0.51)
+    p_mc.SetX2NDC(0.95)
+    p_mc.SetY2NDC(0.71)
+    p_mc.Draw()
+
+    p_mc_20wls = h_mc_20wls.GetListOfFunctions().FindObject("stats")
+    p_mc_20wls.SetTextColor(kRed + 2)
+    p_mc_20wls.SetLineColor(kRed + 2)
+    p_mc_20wls.SetX1NDC(0.72)
+    p_mc_20wls.SetY1NDC(0.27)
+    p_mc_20wls.SetX2NDC(0.95)
+    p_mc_20wls.SetY2NDC(0.47)
+    p_mc_20wls.Draw()
+
+    c1.Update()
+    c1.SaveAs('figures/plot_effect_of_20_wls.pdf')
     raw_input('Press anykey to continue.\n')
+
+def plot_data_vs_mc(h_data, h_mc, x_title, y_title, name_data, name_mc, figure_filename):
+    c1 = TCanvas('c1', 'c1', 800, 600)
+    set_margin()
+    h_data.SetLineColor(kRed + 2)
+    h_data.GetXaxis().SetRangeUser(0, 300)
+    h_data.GetYaxis().SetRangeUser(0, max(h_data.GetMaximum(), h_mc.GetMaximum()) * 1.2)
+    h_data.GetXaxis().SetTitle(x_title)
+    h_data.GetYaxis().SetTitle(y_title)
+    set_h1_style(h_data)
+    h_data.SetName(name_data)
+    h_data.Draw()
+
+    set_h1_style(h_mc)
+    h_mc.SetName(name_mc)
+    h_mc.Draw('sames')
+
+    c1.Update()
+    p_data = h_data.GetListOfFunctions().FindObject("stats")
+    p_data.SetTextColor(kRed + 2)
+    p_data.SetLineColor(kRed + 2)
+    p_data.SetX1NDC(0.72)
+    p_data.SetY1NDC(0.75)
+    p_data.SetX2NDC(0.95)
+    p_data.SetY2NDC(0.95)
+    p_data.Draw()
+
+    p_mc = h_mc.GetListOfFunctions().FindObject("stats")
+    p_mc.SetTextColor(kBlue + 2)
+    p_mc.SetLineColor(kBlue + 2)
+    p_mc.SetX1NDC(0.72)
+    p_mc.SetY1NDC(0.51)
+    p_mc.SetX2NDC(0.95)
+    p_mc.SetY2NDC(0.71)
+    p_mc.Draw()
+
+    c1.Update()
+    c1.SaveAs(figure_filename)
+    raw_input('Press anykey to continue.\n')
+
 
 def plot_effect_of_removing_cell_hit():
-    h_mc_slice_duration = TH1D('h_mc_slice_duration', 'h_mc_slice_duration', 100, 0, 500)
-    h_mc_slice_duration_minus_1 = TH1D('h_mc_slice_duration_minus_1', 'h_mc_slice_duration_minus_1', 100, 0, 500)
-    h_mc_slice_duration_minus_2 = TH1D('h_mc_slice_duration_minus_2', 'h_mc_slice_duration_minus_2', 100, 0, 500)
-    h_mc_slice_duration_minus_3 = TH1D('h_mc_slice_duration_minus_3', 'h_mc_slice_duration_minus_3', 100, 0, 500)
-    h_mc_slice_duration_minus_4 = TH1D('h_mc_slice_duration_minus_4', 'h_mc_slice_duration_minus_4', 100, 0, 500)
-    for slice_tns in get_slice_tnss('tns41.txt'):
-        slice_tns = sorted(slice_tns)
-        h_mc_slice_duration.Fill(max(slice_tns) - min(slice_tns))
-        h_mc_slice_duration_minus_1.Fill(max(slice_tns[1:-1]) - min(slice_tns[1:-1]))
-        h_mc_slice_duration_minus_2.Fill(max(slice_tns[2:-2]) - min(slice_tns[2:-2]))
-        # h_mc_slice_duration_minus_3.Fill(max(slice_tns[3:-3]) - min(slice_tns[3:-3]))
-        # h_mc_slice_duration_minus_4.Fill(max(slice_tns[4:-4]) - min(slice_tns[4:-4]))
-    event_count_mc = float(len(get_event_tnss('tns41.txt')))
+    gStyle.SetOptStat('nmri')
 
-    f1 = TFile('photon_timing.root')
-    h_data_slice_count = f1.Get('photontimingana/fSliceCount')
-    event_count_data = h_data_slice_count.GetEntries()
-    h_data_slice_duration = f1.Get('photontimingana/fSliceDuration')
-    h_data_slice_duration_minus_1 = f1.Get('photontimingana/fSliceDurationMinus1')
-    h_data_slice_duration_minus_2 = f1.Get('photontimingana/fSliceDurationMinus2')
-    h_data_slice_duration_minus_3 = f1.Get('photontimingana/fSliceDurationMinus3')
-    h_data_slice_duration_minus_4 = f1.Get('photontimingana/fSliceDurationMinus4')
+    f_mc = TFile('data/photon_timing_mc.45.root')
+    h_mc_slice_duration = f_mc.Get('photontimingana/fSliceDuration')
+    h_mc_slice_duration_minus_1 = f_mc.Get('photontimingana/fSliceDurationMinus1')
+    h_mc_slice_duration_minus_2 = f_mc.Get('photontimingana/fSliceDurationMinus2')
+    h_slice_count = f_mc.Get('photontimingana/fSliceCount')
+    mc_event_count = h_slice_count.GetEntries()
 
-    h_data_slice_duration.Scale(event_count_mc / event_count_data)
-    h_data_slice_duration_minus_1.Scale(event_count_mc / event_count_data)
-    h_data_slice_duration_minus_2.Scale(event_count_mc / event_count_data)
-    h_data_slice_duration_minus_3.Scale(event_count_mc / event_count_data)
-    h_data_slice_duration_minus_4.Scale(event_count_mc / event_count_data)
+    f_data = TFile('data/photon_timing.root')
+    h_data_slice_duration = f_data.Get('photontimingana/fSliceDuration')
+    h_data_slice_duration_minus_1 = f_data.Get('photontimingana/fSliceDurationMinus1')
+    h_data_slice_duration_minus_2 = f_data.Get('photontimingana/fSliceDurationMinus2')
+    h_slice_count = f_data.Get('photontimingana/fSliceCount')
+    data_event_count = h_slice_count.GetEntries()
 
+    print 'mc_event_count = ', mc_event_count
+    print 'data_event_count = ', data_event_count
 
-    gStyle.SetOptStat(0)
-    # c0 = TCanvas('c0', 'c0', 800, 600)
-    # set_margin()
-    # h_data_slice_duration.SetLineColor(kRed + 1)
-    # h_data_slice_duration.GetXaxis().SetRangeUser(0, 300)
-    # h_data_slice_duration.GetYaxis().SetRangeUser(0, max(h_data_slice_duration.GetMaximum(), h_mc_slice_duration.GetMaximum()) * 1.2)
-    # h_data_slice_duration.GetXaxis().SetTitle('Slice Duration (ns)')
-    # h_data_slice_duration.GetYaxis().SetTitle('Slice Count')
-    # set_h1_style(h_data_slice_duration)
-    # h_data_slice_duration.Draw()
+    h_data_slice_duration.Scale(mc_event_count / data_event_count)
+    h_data_slice_duration_minus_1.Scale(mc_event_count / data_event_count)
+    h_data_slice_duration_minus_2.Scale(mc_event_count / data_event_count)
 
-    # set_h1_style(h_mc_slice_duration)
-    # h_mc_slice_duration.Draw('sames')
+    plot_data_vs_mc(h_data_slice_duration, h_mc_slice_duration,
+                    'Slice Duration (ns)', 'Slice Count',
+                    'Data', 'MC', 'figures/plot_effect_of_removing_cell_hit.pdf')
 
-    # c0.Update()
-    # c0.SaveAs('figures/plot_effect_of_removing_cell_hit.pdf')
+    plot_data_vs_mc(h_data_slice_duration_minus_1, h_mc_slice_duration_minus_1,
+                    'Slice Duration (ns)', 'Slice Count',
+                    'Data', 'MC', 'figures/plot_effect_of_removing_cell_hit_minus_1.pdf')
 
-    # c1 = TCanvas('c1', 'c1', 800, 600)
-    # set_margin()
-    # h_data_slice_duration_minus_1.SetLineColor(kRed + 1)
-    # h_data_slice_duration_minus_1.GetXaxis().SetRangeUser(0, 300)
-    # h_data_slice_duration_minus_1.GetYaxis().SetRangeUser(0, max(h_data_slice_duration_minus_1.GetMaximum(), h_mc_slice_duration_minus_1.GetMaximum()) * 1.2)
-    # h_data_slice_duration_minus_1.GetXaxis().SetTitle('Slice Duration (ns)')
-    # h_data_slice_duration_minus_1.GetYaxis().SetTitle('Slice Count')
-    # set_h1_style(h_data_slice_duration_minus_1)
-    # h_data_slice_duration_minus_1.Draw()
+    plot_data_vs_mc(h_data_slice_duration_minus_2, h_mc_slice_duration_minus_2,
+                    'Slice Duration (ns)', 'Slice Count',
+                    'Data', 'MC', 'figures/plot_effect_of_removing_cell_hit_minus_2.pdf')
 
-    # set_h1_style(h_mc_slice_duration_minus_1)
-    # h_mc_slice_duration_minus_1.Draw('sames')
-
-    # c1.Update()
-    # c1.SaveAs('figures/plot_effect_of_removing_cell_hit_minus_1.pdf')
-
-    c2 = TCanvas('c2', 'c2', 800, 600)
-    set_margin()
-    h_data_slice_duration_minus_2.SetLineColor(kRed + 1)
-    h_data_slice_duration_minus_2.GetXaxis().SetRangeUser(0, 300)
-    h_data_slice_duration_minus_2.GetYaxis().SetRangeUser(0, max(h_data_slice_duration_minus_2.GetMaximum(), h_mc_slice_duration_minus_2.GetMaximum()) * 1.2)
-    h_data_slice_duration_minus_2.GetXaxis().SetTitle('Slice Duration (ns)')
-    h_data_slice_duration_minus_2.GetYaxis().SetTitle('Slice Count')
-    set_h1_style(h_data_slice_duration_minus_2)
-    h_data_slice_duration_minus_2.Draw()
-
-    set_h1_style(h_mc_slice_duration_minus_2)
-    h_mc_slice_duration_minus_2.Draw('sames')
-
-    c2.Update()
-    c2.SaveAs('figures/plot_effect_of_removing_cell_hit_minus_2.pdf')
-
-    raw_input('Press anykey to continue.\n')
 
 # pprint(sorted(map(lambda x: len(x), get_slice_tnss('tns21.txt'))))
 # pprint(sorted(map(lambda x: len(x), get_slice_tnss('tns23.txt'))))
 # pprint(sorted(map(lambda x: len(x), get_slice_tnss('tns20.txt'))))
-
 # pprint(len(sorted(map(lambda x: len(x), get_slice_tnss('tns36.txt')))))
 # pprint(len(sorted(map(lambda x: len(x), get_slice_tnss('tns35.txt')))))
 # pprint(len(sorted(map(lambda x: len(x), get_slice_tnss('tns29.txt')))))
-
 # pprint(sum(map(lambda x: len(x), get_slice_tnss('tns30.txt'))))
 # pprint(sum(map(lambda x: len(x), get_slice_tnss('tns29.txt'))))
 # pprint(get_slice_tnss('tns20.txt'))
@@ -279,5 +309,5 @@ def plot_effect_of_removing_cell_hit():
 # plot_slice_durations()
 # plot_cell_tns()
 # plot_h1_absolute_cell_tns()
-plot_slice_durations_from_hist()
-# plot_effect_of_removing_cell_hit()
+# plot_effect_of_20_wls()
+plot_effect_of_removing_cell_hit()
