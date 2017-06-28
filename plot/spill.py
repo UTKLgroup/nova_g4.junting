55d636963a3122d92f86d7ab5c75d0042ad379fc
juntingh_imac <junting.huang@gmail.com>
2017-06-27 19:00:22 -0500

    plot_tns_distribution in 4 squares

--- plot/spill.py
+++ plot/spill.py
@@ -5,59 +5,81 @@ f_mc = TFile('photon_timing.nd_spill_mc.root')
 figure_dir = '/Users/juntinghuang/google_drive/slides/beamer/20170621_cosmic_timing/figures'
 
 def plot_tns_distribution(hist_name):
-    h_data = f_data.Get('photontimingspillana/{}'.format(hist_name))
-    h_mc = f_mc.Get('photontimingspillana/{}'.format(hist_name))
-    h_data.Scale(1.0 / h_data.Integral())
-    h_mc.Scale(1.0 / h_mc.Integral())
+    hist_names = ['fSliceCellHitTns1st', 'fSliceCellHitTns2nd', 'fSliceCellHitTns3rd', 'fSliceCellHitTns4th']
+    x_titles = ['Time to 1st Hit (ns)',
+                'Time to 2nd Hit (ns)',
+                'Time to 3rd Hit (ns)',
+                'Time to 4th Hit (ns)']
+    h_datas = []
+    h_mcs = []
+    for hist_name in hist_names:
+        h_data = f_data.Get('photontimingspillana/{}'.format(hist_name))
+        h_data.Scale(1.0 / h_data.Integral())
+        set_h1_style(h_data)
+        h_data.SetLineColor(kRed)
+        h_datas.append(h_data)
 
-    c1 = TCanvas('c1', 'c1', 800, 600)
-    set_margin()
-    set_h1_style(h_data)
-    h_data.SetLineColor(kRed)
-    h_data.GetXaxis().SetRangeUser(1, 500)
-    h_data.GetYaxis().SetRangeUser(0, max(h_data.GetMaximum(), h_mc.GetMaximum()) * 1.2)
-    h_data.SetName('FD Cosmic Data')
-    h_data.GetXaxis().SetTitle('Time to First Selected Cell Hit (ns)')
-    h_data.GetYaxis().SetTitle('Cell Hit Count')
-    h_data.GetYaxis().SetTitleOffset(1.6)
-    h_data.Draw('hist')
+        h_mc = f_mc.Get('photontimingspillana/{}'.format(hist_name))
+        h_mc.Scale(1.0 / h_mc.Integral())
+        set_h1_style(h_mc)
+        h_mcs.append(h_mc)
 
-    set_h1_style(h_mc)
-    h_mc.SetName('FD Cosmic MC')
-    h_mc.Draw('sames, hist')
+    gStyle.SetOptStat('nmr')
+    c1 = TCanvas('c1', 'c1', 1200, 800)
+    c1.Divide(2, 2)
+    for i in range(4):
+        c1.cd(i + 1)
+        h_data = h_datas[i]
+        h_mc = h_mcs[i]
+        h_data.SetName('data')
+        h_mc.SetName('MC')
 
-    c1.Update()
-    p_data = h_data.GetListOfFunctions().FindObject("stats")
-    p_data.SetTextColor(h_data.GetLineColor())
-    p_data.SetLineColor(h_data.GetLineColor())
-    p_data.SetX1NDC(0.7)
-    p_data.SetY1NDC(0.75)
-    p_data.SetX2NDC(0.95)
-    p_data.SetY2NDC(0.95)
-    p_data.Draw()
+        gPad.SetTopMargin(0.05)
+        gPad.SetBottomMargin(0.2)
+        gPad.SetLeftMargin(0.15)
+        h_mc.Rebin(2)
+        h_data.Rebin(2)
 
-    p_mc = h_mc.GetListOfFunctions().FindObject("stats")
-    p_mc.SetTextColor(h_mc.GetLineColor())
-    p_mc.SetLineColor(h_mc.GetLineColor())
-    p_mc.SetX1NDC(0.7)
-    p_mc.SetY1NDC(0.51)
-    p_mc.SetX2NDC(0.95)
-    p_mc.SetY2NDC(0.71)
-    p_mc.Draw()
+        h_mc.Draw('hist')
+        h_mc.GetYaxis().SetTitle('Cell Hit Count')
+        h_mc.GetXaxis().SetTitle(x_titles[i])
+        h_mc.GetXaxis().SetTitleOffset(2.2)
+        h_mc.GetYaxis().SetTitleOffset(2.3)
+        h_mc.GetXaxis().SetRangeUser(0, 200)
+        h_data.Draw('hist,sames')
 
-    c1.Update()
-    c1.SaveAs('{}/spill_plot_tns_distribution.{}.pdf'.format(figure_dir, hist_name))
-    raw_input('Press any key to continue.')
+        c1.Update()
+        p_data = h_data.GetListOfFunctions().FindObject("stats")
+        p_data.SetTextColor(h_data.GetLineColor())
+        p_data.SetLineColor(h_data.GetLineColor())
+        p_data.SetX1NDC(0.62)
+        p_data.SetY1NDC(0.68)
+        p_data.SetX2NDC(0.85)
+        p_data.SetY2NDC(0.88)
+        p_data.Draw()
+
+        p_mc = h_mc.GetListOfFunctions().FindObject("stats")
+        p_mc.SetTextColor(h_mc.GetLineColor())
+        p_mc.SetLineColor(h_mc.GetLineColor())
+        p_mc.SetX1NDC(0.62)
+        p_mc.SetY1NDC(0.44)
+        p_mc.SetX2NDC(0.85)
+        p_mc.SetY2NDC(0.64)
+        p_mc.Draw()
+
+        c1.Update()
+
+    c1.SaveAs('{}/spill_plot_tns_distribution.pdf'.format(figure_dir))
+    raw_input('Press anykey to continue.\n')
     
 
 def plot_gev_distribution(hist_name):
-    # hist_name = 'fSliceRecoHitGeV'
     h_data = f_data.Get('photontimingspillana/{}'.format(hist_name))
     h_mc = f_mc.Get('photontimingspillana/{}'.format(hist_name))
     h_data.Scale(1.0 / h_data.Integral())
     h_mc.Scale(1.0 / h_mc.Integral())
 
-    c1 = TCanvas('c1', 'c1', 800, 600)
+    c1 = TCanvas('c1', 'c1', 800, 800)
     set_margin()
     set_h1_style(h_data)
     h_data.SetLineColor(kRed)
@@ -175,7 +197,7 @@ def plot_slice_duration(hist_name):
     c1.SaveAs('{}/spill_plot_slice_duration.{}.pdf'.format(figure_dir, hist_name))
     raw_input('Press any key to continue.')
 
-# plot_tns_distribution('fSliceCellHitTns1st')
+plot_tns_distribution('fSliceCellHitTns1st')
 # plot_tns_distribution('fSliceCellHitTns2nd')
 # plot_tns_distribution('fSliceCellHitTns3rd')
 # plot_tns_distribution('fSliceCellHitTns4th')
@@ -187,4 +209,4 @@ def plot_slice_duration(hist_name):
 # plot_slice_duration('fSliceDuration')
 # plot_slice_duration('fSliceDuration80')
 # plot_slice_duration('fSliceDuration60')
-plot_slice_duration('fSliceDurationSmallTimeWindow')
+# plot_slice_duration('fSliceDurationSmallTimeWindow')
